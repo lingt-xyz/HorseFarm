c88470aa7832b6686bcbf5b3297a522ff53596e8
Ling Tan <lingtan.ca@gmail.com>
2018-04-12 04:52:30 -0400

    update shadow

--- src/horse_farm.cc
+++ src/horse_farm.cc
@@ -170,7 +170,7 @@ void HorseFarm::Render()
         // --------------------------------------------------------------
         //// note that if you use a perspective projection matrix you'll have to change the light position as the current light position isn't enough to reflect the whole scene
         glm::mat4 lightProjection = glm::perspective(glm::radians(130.0f), (GLfloat)SHADOW_WIDTH / (GLfloat)SHADOW_HEIGHT, 1.0f, 100.0f);
-        lightProjection = glm::ortho(-30.0f, 30.0f, -30.0f, 30.0f, 1.0f, 100.0f);
+        //lightProjection = glm::ortho(-30.0f, 30.0f, -30.0f, 30.0f, 1.0f, 100.0f);
         glm::mat4 lightView = glm::lookAt(Controller::light_position_, glm::vec3(0.0f), glm::vec3(0.0, 0.0, 1.0));
         lightSpaceMatrix = lightProjection * lightView;
         // render scene from light's point of view
